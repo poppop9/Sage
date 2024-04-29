5c6ba095dbbaa5fd5307838be0d1630d45b10778
123 <1962883041@qq.com>
2024-04-29 20:41:28 +0800

    测试鉴权

--- Sage-interfaces/src/main/java/app/xlog/ggbond/App.java
+++ Sage-interfaces/src/main/java/app/xlog/ggbond/App.java
@@ -1,10 +1,10 @@
 package app.xlog.ggbond;
 
-import app.xlog.ggbond.utils.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,13 +21,18 @@ public class App {
     }
 
     @GetMapping("/verify")
-    public Result<String> verify(String token) {
+    public ResponseEntity<String> verify(String token) {
         logger.atInfo().log("token: {}", token);
         if (token.equals("success")) {
             logger.atInfo().log("请求成功");
-            return Result.buildResult(Result.Status.OK, "success");
+            return ResponseEntity.ok("success");
         } else {
-            return Result.buildResult(Result.Status.ERROR);
+            return ResponseEntity.status(403).body("forbidden");
         }
+    }
+
+    @GetMapping("/aftersuccess")
+    public String aftersuccess() {
+        return "验证通过，可以访问网站";
     }
 }
